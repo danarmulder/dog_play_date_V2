6eb9f3cba05bb8d213359af6944be2687a302634
Dana Mulder <danarmulder@gmail.com>
2015-03-03 12:52:35 -0800

    add authentication and registration specs

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+feature "Sign In" do
+  scenario "User signs in" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "barbarastreisand@aol.com"
+    fill_in "Password", with: "1234"
+    click_on "signing-user-in-action"
+
+
+    expect(page).to have_content("Barbara Streisand")
+    expect(page).to have_content("Sign Out")
+  end
+
+  scenario "User signs in with wrong password and is not permitted" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "barbarastreisand@aol.com"
+    fill_in "Password", with: "123"
+    click_on "signing-user-in-action"
+
+
+    expect(page).to have_no_content("Sign Out")
+    expect(page).to have_content("Username / password combination is invalid")
+  end
+
+  scenario "User signs in with wrong email and is not permitted" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "b-barbarastreisand@aol.com"
+    fill_in "Password", with: "1234"
+    click_on "signing-user-in-action"
+
+
+    expect(page).to have_no_content("Sign Out")
+    expect(page).to have_content("Username / password combination is invalid")
+  end
+
+  scenario "User tries to sign in with blank fields and is not permitted" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: " "
+    fill_in "Password", with: " "
+    click_on "signing-user-in-action"
+
+
+    expect(page).to have_no_content("Sign Out")
+    expect(page).to have_content("Username / password combination is invalid")
+  end
+end
+
+feature "Sign Out" do
+  scenario "A user can sign out" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "barbarastreisand@aol.com"
+    fill_in "Password", with: "1234"
+    click_on "signing-user-in-action"
+
+    expect(page).to have_content("Sign Out")
+    click_on "Sign Out"
+
+    expect(page).to have_no_content("Sign Out")
+    expect(page).to have_content("Sign In")
+  end
+end
---
+++ spec/features/registrations_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+feature "Sign-Up" do
+  scenario "User signs up" do
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Robert"
+    fill_in "Last name", with: "Downey Jr."
+    fill_in "Email", with: "sherlockholmes@yahoo.com"
+    fill_in "Location", with: "94117"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_on "Sign up"
+
+    expect(page).to have_content("Robert Downey Jr.")
+    expect(page).to have_content("Sign Out")
+  end
+
+  scenario "User signs up with non-mathing passwords and is not permitted" do
+    visit root_path
+    click_on "Sign Up"
+    ill_in "First name", with: "Robert"
+    fill_in "Last name", with: "Downey Jr."
+    fill_in "Email", with: "sherlockholmes@yahoo.com"
+    fill_in "Location", with: "94117"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_on "Sign up"
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+
+  scenario "User signs up with an email thats taken and is not permitted" do
+    user = User.create!(
+      first_name: "Barbara",
+      last_name: "Streisand",
+      location: "94117",
+      email: "barbarastreisand@aol.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
+
+
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Barbara"
+    fill_in "Last name", with: "Streisand"
+    fill_in "Email", with: "barbarastreisand@aol.com"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "123"
+    click_on "Sign up"
+
+    expect(page).to have_content("Email has already been taken")
+  end
+
+end
