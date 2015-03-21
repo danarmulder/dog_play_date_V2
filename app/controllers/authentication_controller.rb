b26ce1b35023d952cc6904031bf16c836463ae82
roryg11 <Rory.C.Grant@gmail.com>
2015-03-21 12:15:32 -0700

    authentication specs passing

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,15 @@
 class AuthenticationController < ApplicationController
 
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      @sign_in_error = "Email/password combination is invalid"
+      render :new
+    end
+  end
 
   def destroy
     session.clear
