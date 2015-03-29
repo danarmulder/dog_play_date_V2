68f7c1f2718b1ff7bf382930ba4202f12efb91fa
roryg11 <Rory.C.Grant@gmail.com>
2015-03-28 22:57:18 -0700

    adding a show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @dogs = @user.dogs
+  end
+
   private
   def user_params
     params.require(:user).permit(
