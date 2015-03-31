8f44b082b346c99e155dfb89b6c2006b6204a17b
roryg11 <Rory.C.Grant@gmail.com>
2015-03-31 09:49:50 -0700

    validating that a user can only have one gender filter

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,5 +1,13 @@
 class Gender < Filter
+  validate :user_cannot_have_more_than_one_gender_filter
+
   def filter(dogs)
     dogs.where({gender: "#{content}"})
+  end
+
+  def user_cannot_have_more_than_one_gender_filter
+    if Filter.where(user_id: user_id, type: "Gender").exists?
+      errors.add(:user_id, "Cannot have more than one gender preference")
+    end
   end
 end
