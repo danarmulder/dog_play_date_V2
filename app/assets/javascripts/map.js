1c3b81c69244c06d59b43f2639bd217c95fc6fa4
Dana Mulder <danarmulder@gmail.com>
2015-04-02 12:48:19 -0700

    attempt to fix map load issue

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,28 +1,40 @@
+dogParksCollection = [];
+
 $(document).ready(function(){
-  var dogParksCollection = [];
-  $.ajax({
-    type: "get",
-    url: 'https://apricot-crisp-1097.herokuapp.com/geojson',
-    dataType:"json",
-  }).done(function(results) {
-    dogParksCollection = results[0];
+  if(dogParksCollection.length === 0){
+    console.log('no dog parks there');
+    $.ajax({
+      type: "get",
+      url: 'https://apricot-crisp-1097.herokuapp.com/geojson',
+      dataType:"json",
+    }).done(function(results) {
+      dogParksCollection = results[0];
+      console.log('ajax call done');
+      buildmap();
+    });
+  } else {
+    console.log(dogParksCollection);
     buildmap();
-  });
+  }
   function buildmap(){
     L.mapbox.accessToken = 'pk.eyJ1IjoiZGF5eW51aGhoIiwiYSI6IlNrUWlXd0kifQ.PkwjuKO6Clksu2OGIoePeA';
-    $( "#mapbox_map" ).load(mapBox());
+    $('#mapbox_map').load(mapBox());
+    console.log('in buildmap');
   }
 
   function mapBox(){
+
+    console.log('in mapbox');
     var latitude = $('#mapbox_map')[0].dataset.userlatitude;
     var longitude = $('#mapbox_map')[0].dataset.userlongitude;
     var otherlat = $('#mapbox_map')[0].dataset.otherlatitude;
     var otherlong = $('#mapbox_map')[0].dataset.otherlongitude;
-    var map = L.mapbox.map('mapbox_map', 'examples.map-i86nkdio').setView([latitude, longitude], 13);
-
+    var map = L.mapbox.map('mapbox_map', 'examples.map-i86nkdio').setView([37.7822891, -122.463708], 13);
+    console.log('map built');
     map.on('ready', function(){
       $('#mapbox_map .active').removeClass('active');
     });
+
     var featureArray = [];
 
     featureArray.push({
